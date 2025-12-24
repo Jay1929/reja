e68b630f3c7d766ce002225aee49d055398c66f5
JAY <jay.path07@gmail.com>
2025-12-24 17:16:12 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,33 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing
+app.get("/", function (req, res) {
+  res.end(`<h1 style="background: red">HELLO WORLD by JAY</h1>`);
+});
+
+app.get("/hello", function (req, res) {
+  res.end("Salom");
+});
+
+app.get("/gift", function (req, res) {
+  res.end("Siz sovgalar sahifasidasiz");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`The server is running succesfully on port: ${PORT}`);
+});
