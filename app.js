48e0720b89aeab5b521fa00063e96b999a63cd7a
JAY <jay.path07@gmail.com>
2025-12-31 17:33:01 +0900

    feat: MongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,58 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+
+// const fs = require("fs");
+
+// Mongo DB chaqirish
+const db = require("./server").db();
+
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//   if(err) {
+//     console.log("ERROR:", err);
+//   } else {
+//     user = JSON.parse(data)
+//   }  
+// });
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true })); 
+ 
+// 2: Session code  
+// 3: Views code 
+app.set("views", "views"); 
+app.set("view engine", "ejs");  
+
+// 4: Routing
+// app.get("/", function (req, res) {
+//   res.end(`<h1 style="background: red">HELLO WORLD by JAY</h1>`);
+// });          
+
+// app.get("/hello", function (req, res) {
+ 
+//   res.end("Salom");    
+// });    
+  
+// app.get("/gift", function (req, res) {
+//   res.end("Siz sovgalar sahifasidasiz");  
+// });
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "succes"});  
+});   
+// app.get("/author", (req, res) => {
+//   res.render("author", {user: user}); 
+// }); 
+// app.get("/", (req, res) => { 
+//   res.redirect("/author");
+// });
+
+app.get("/", function (req, res) {
+  res.render("reja");   
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,59 +1,29 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data)
+let db;
+const connectString = "mongodb+srv://janob_Jay:6Y8oSwhZKx881AlL@cluster0.xfidt4d.mongodb.net/"
+
+mongodb.connect(
+connectString, 
+{
+  useNewUrl: true, 
+  useUnifiedTopology: true,
+}, 
+(err, client) => {
+  if(err) console.log("ERROR on connection MongoDB");
+  else {
+    console.log("MongoDB connection suceed");
+    module.exports = client;
+    const app = require("./app");
+    const server = http.createServer(app); 
+    let PORT = 3000;
+    server.listen(PORT, function () {
+      console.log(
+        `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+      );       
+    });
+   }  
   }
-});
+);
 
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true })); 
- 
-// 2: Session code  
-// 3: Views code 
-app.set("views", "views"); 
-app.set("view engine", "ejs");  
-
-// 4: Routing
-// app.get("/", function (req, res) {
-//   res.end(`<h1 style="background: red">HELLO WORLD by JAY</h1>`);
-// });          
-
-// app.get("/hello", function (req, res) {
- 
-//   res.end("Salom");    
-// });    
-  
-// app.get("/gift", function (req, res) {
-//   res.end("Siz sovgalar sahifasidasiz");  
-// });
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "succes"});  
-});   
-app.get("/author", (req, res) => {
-  res.render("author", {user: user}); 
-}); 
-// app.get("/", (req, res) => { 
-//   res.redirect("/author");
-// });
-
-app.get("/reja", function (req, res) {
-  res.render("reja");   
-});
-
-const server = http.createServer(app); 
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);       
-});
